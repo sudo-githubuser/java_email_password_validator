5217d3931798956d55348aff687941d5f425f23d
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2025-11-02 23:34:54 +0530

    Basic working. Need to refine the logic and methods

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Enter your email: ");
-
+        Helper helper = new Helper();
+        String email = helper.get_email_input();
+        String password = helper.get_password_input();
+        if (Validator.isValidEmail(email) && Validator.isValidPassword(password)) {
+            System.out.println("Username and password set successfully");
+        } else if (!Validator.isValidPassword(password)) {
+            System.out.println("Password is not valid");
+        } else if (!Validator.isValidEmail(email)) {
+            System.out.println("Email is not valid");
         }
     }
+}
