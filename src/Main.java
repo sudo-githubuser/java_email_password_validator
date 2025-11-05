3920af46d874fbb239535b55ebea476a3621d824
Praveen S <51072726+i-ampraveen@users.noreply.github.com>
2025-11-05 19:51:58 +0530

    Improved Email and password logic. Writing valid emali address to a text file

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,12 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Main {
@@ -9,10 +18,37 @@ public class Main {
         System.out.println("--- Account Creation ---");
 
         String email = helper.get_email_input(scanner);
+        String password = helper.get_password_input(scanner);
 
         System.out.println("\n--- Success! ---");
         System.out.println("The valid email you entered is: " + email);
+        System.out.println("The password you entered is valid");
 
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String timeStamp = now.format(formatter);
+
+//        try(
+//                BufferedWriter writer = new BufferedWriter(new FileWriter("loginDetails.txt", true))
+//        ){
+//
+//            writer.write("Email: " + email + " created at: " + "["+timeStamp+"]");
+//            writer.newLine();
+//
+//        } catch (IOException e) {
+//            System.out.println("Error writing to file: " + e.getMessage());
+//        }
+
+        try{
+            Files.writeString(
+                    Path.of("loginDetails.txt"),
+                    "Email: " + email + " created at: " + timeStamp + System.lineSeparator(),
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND
+            );
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
         scanner.close();
 
     }
--- src/Validator.java
+++ src/Validator.java
@@ -1,9 +1,17 @@
 public class Validator {
     public static boolean isValidEmail(String email) {
+        if (email == null || email.length() > 254) {
+            return false;
+        }
+
         return email.contains("@") && (email.endsWith(".com") || email.endsWith(".in") || email.endsWith(".org"));
     }
 
     public static boolean isValidPassword(String password) {
+        if (password == null || password.length() < 8 || password.length() > 40) {
+            return false;
+        }
+
         return password.length() > 8
                 && password.matches(".*[A-Z].*")
                 && password.matches(".*[a-z].*")
